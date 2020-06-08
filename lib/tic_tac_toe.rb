e006ff73a07af98567754d6134be5fdf3411bc07
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-06-08 23:28:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,6 +15,7 @@ class TicTacToe
   [0,4,8],]
   
   def display_board
+    
   end
   
 end
@@ -58,13 +59,13 @@ end
 #     [2,4,6]
 #   ]
 
-#   def display_board
-#     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-#     puts "-----------"
-#     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-#     puts "-----------"
-#     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-#   end
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 
 #   def input_to_index(user_input)
 #     user_input.to_i - 1
