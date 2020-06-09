5c7db7380585bd8b7c3cf3fd4cfe3da366abd737
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-06-09 01:02:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,11 +55,11 @@ class TicTacToe
   end
   
   def turn
-    puts"hey user whats your choice?"
-    choice = gets.strip
-    position = input_to_index(choice)
-    if valid_move?(position)
-      move(position,current_player)
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      move(index, current_player)
       display_board
     else
       turn
@@ -141,17 +141,17 @@ end
   #   turn_count % 2 == 0 ? "X" : "O"
   # end
 
-  def turn
-    puts "Please enter 1-9:"
-    user_input = gets.strip
-    index = input_to_index(user_input)
-    if valid_move?(index)
-      move(index, current_player)
-      display_board
-    else
-      turn
-    end
-  end
+  # def turn
+  #   puts "Please enter 1-9:"
+  #   user_input = gets.strip
+  #   index = input_to_index(user_input)
+  #   if valid_move?(index)
+  #     move(index, current_player)
+  #     display_board
+  #   else
+  #     turn
+  #   end
+  # end
 
 #   def won?
 #     WIN_COMBINATIONS.detect do |combo|
