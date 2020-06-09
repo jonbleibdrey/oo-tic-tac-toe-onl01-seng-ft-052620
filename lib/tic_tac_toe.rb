950cdb3fe05bf2c0aafc0fb6bd8868b795c46778
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-06-09 01:01:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -141,17 +141,17 @@ end
   #   turn_count % 2 == 0 ? "X" : "O"
   # end
 
-#   def turn
-#     puts "Please enter 1-9:"
-#     user_input = gets.strip
-#     index = input_to_index(user_input)
-#     if valid_move?(index)
-#       move(index, current_player)
-#       display_board
-#     else
-#       turn
-#     end
-#   end
+  def turn
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
 
 #   def won?
 #     WIN_COMBINATIONS.detect do |combo|
